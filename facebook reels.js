a6928e11283d1d4671aa01946e3518977407c3e4
khaledz370 <khaledzaki370@gmail.com>
2025-06-19 12:45:01 +0300

    Update facebook reels.js

--- facebook reels.js
+++ facebook reels.js
@@ -4,7 +4,7 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *.facebook.com/*
+// @match        *.facebook.com/reel/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=facebook.com
 // @grant        none
 // @require      https://code.jquery.com/jquery-3.7.1.min.js
